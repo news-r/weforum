67e5b008aea7e6a19f44678f054f9e795ad6eee3
John Coene <jcoenep@gmail.com>
2019-06-24 11:59:12 +0800

    info msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   options(weforum.base_url = "https://api.weforum.org/")
+  packageStartupMessage(
+    crayon::blue(cli::symbol$info), " No API key is required."
+  )
 }
 
 .onLoad <- function(libname, pkgname) {
---
+++ parse.R
@@ -0,0 +1,9 @@
+#' Parse results
+#' 
+#' @export
+wef_parse <- function(data) {
+  if(!inherits(data, "list"))
+    stop("data must be a list", call. = FALSE)
+
+  
+}
